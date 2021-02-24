5bc95e3c0bcd15f7f7277b4d8bc5464a156fe647
winston-lim <131517e@student.hci.edu.sg>
2021-02-24 23:13:10 +0800

    Add splashscreen when snapshot.hasData

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import './screens/home/home_screen.dart';
+import './screens/splash_screen.dart';
 import './screens/wrapper.dart';
 
 void main() {
@@ -11,10 +13,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
+    return MaterialApp(
+      home: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return HomeScreen();
+          }
           return StreamProvider<User>.value(
             value: FirebaseAuth.instance.authStateChanges(),
             child: MaterialApp(
@@ -22,11 +30,8 @@ class MyApp extends StatelessWidget {
               home: Wrapper(),
             ),
           );
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
+        },
+      ),
     );
   }
 }
